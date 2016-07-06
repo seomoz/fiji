6afd91f236966942503bceffac65636b19b559f4
Phil Smith <phil.h.smith@gmail.com>
2016-07-06 12:31:31 -0700

    Pass gpg properties on command line.
    This makes them unquestionably available to everything in maven in all scenarios.

--- devtools/src/main/python/wibi/build/workflow_task.py
+++ devtools/src/main/python/wibi/build/workflow_task.py
@@ -368,56 +368,6 @@ POM_XML_COMMON_CONFIGURATION = """\
     <url>git@github.com:seomoz/fiji.git</url>
   </scm>
 
-  <!-- https://github.com/making/travis-ci-maven-deploy-skelton -->
-  <profiles>
-    <profile>
-      <id>ossrh</id>
-      <properties>
-        <gpg.executable>gpg</gpg.executable>
-        <gpg.keyname>${env.GPG_KEYNAME}</gpg.keyname>
-        <gpg.passphrase>${env.GPG_PASSPHRASE}</gpg.passphrase>
-        <gpg.defaultKeyring>false</gpg.defaultKeyring>
-        <gpg.publicKeyring>${env.TRAVIS_BUILD_DIR}/devtools/pubring.gpg</gpg.publicKeyring>
-        <gpg.secretKeyring>${env.TRAVIS_BUILD_DIR}/devtools/secring.gpg</gpg.secretKeyring>
-      </properties>
-      <activation>
-        <property>
-          <name>performRelease</name>
-          <value>true</value>
-        </property>
-      </activation>
-      <build>
-        <plugins>
-          <plugin>
-            <groupId>org.apache.maven.plugins</groupId>
-            <artifactId>maven-gpg-plugin</artifactId>
-            <version>1.5</version>
-            <executions>
-              <execution>
-                <id>sign-artifacts</id>
-                <phase>verify</phase>
-                <goals>
-                  <goal>sign</goal>
-                </goals>
-              </execution>
-            </executions>
-          </plugin>
-          <plugin>
-            <groupId>org.sonatype.plugins</groupId>
-            <artifactId>nexus-staging-maven-plugin</artifactId>
-            <version>1.6.2</version>
-            <extensions>true</extensions>
-            <configuration>
-              <serverId>ossrh</serverId>
-              <nexusUrl>https://oss.sonatype.org/</nexusUrl>
-              <autoReleaseAfterClose>true</autoReleaseAfterClose>
-            </configuration>
-          </plugin>
-        </plugins>
-      </build>
-    </profile>
-  </profiles>
-
   <repositories>
     <repository>
       <id>central</id>
